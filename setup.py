5f3f19b5ead407a3aebeb38e6f7bfebf004dd388
Janne Hellsten <jjhellst@gmail.com>
2021-01-10 13:10:32 +0200

    add install dependency

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ setup(
     long_description="",
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
+    install_requires=[
+        'numpy',
+    ],
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
     cmdclass={"build_ext": build_ext},
