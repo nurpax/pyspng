6e6c5afaab1532ec980323052cad19af14a1326e
Janne Hellsten <jjhellst@gmail.com>
2021-01-10 20:03:25 +0200

    v0.0.3 testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 import sys
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 # Note:
 #   Sort input source files if you glob sources to ensure bit-for-bit
