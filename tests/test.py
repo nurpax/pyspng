759064975448c79c4875e7b9aefe63674bac32f8
Janne Hellsten <jjhellst@gmail.com>
2021-01-10 18:03:45 +0200

    Workaround for missing PIL.Image on CI

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 import os
-import PIL.Image
 
 import pyspng as m
 
@@ -9,16 +8,24 @@ assert m.__version__ == '0.0.1'
 
 fname = os.path.join(os.path.dirname(__file__), 'test.png')
 
+def ref_compare(fn, spngarr):
+    try:
+        import PIL.Image
+        img = PIL.Image.open(fn).convert('RGB')
+        pil_arr = np.array(img)
+
+        # Match that loading the same image with PIL.Image produces
+        # the same bits.
+        assert np.all((spngarr - pil_arr) == 0)
+        print ('PIL image compare ok.')
+    except:
+        print ('Skipping test.  PIL.Image not installed')
+
+
 with open(fname, 'rb') as fin:
     sarr = m.load_png(fin.read())
     print (sarr.shape)
-
-img = PIL.Image.open(fname).convert('RGB')
-pil_arr = np.array(img)
-
-# Match that loading the same image with PIL.Image produces
-# the same bits.
-assert np.all((sarr - pil_arr) == 0)
+    ref_compare(fname, sarr)
 
 try:
     m.load_png(b'this is not a png')
