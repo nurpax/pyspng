dbde0b1ac759ef2965b42d2d471bf5a6fe6d4ca4
Janne Hellsten <jjhellst@gmail.com>
2021-01-10 14:25:02 +0200

    Fix tests for CI

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,14 @@
 
-
 import numpy as np
-import pyspng as m
+import os
 import PIL.Image
+
+import pyspng as m
 
 assert m.__version__ == '0.0.1'
 
-fname = 'test.png'
+fname = os.path.join(os.path.dirname(__file__), 'test.png')
+
 with open(fname, 'rb') as fin:
     sarr = m.load_png(fin.read())
     print (sarr.shape)
@@ -21,4 +23,6 @@ assert np.all((sarr - pil_arr) == 0)
 try:
     m.load_png(b'this is not a png')
 except Exception as e:
-    print (str(e))
+    assert 'could not decode image size' in str(e)
+
+print ('All tests ok.')
